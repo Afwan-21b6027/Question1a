eb4ef44ef221ce28f0b2b40c8bc71bb22158c6d5
Afwan Rezal <afwan.21b6027@gmail.com>
2023-04-10 09:07:08 +0800

    Updates

--- value.php
+++ value.php
@@ -1,13 +1,18 @@
 <?php
     require("db.php");
-    // $carplate = $_POST["carplate"];
-    $carplate = "CA2246";
+    $carplate = $_POST["carplate"];
+    $carplat_err = '<span>Your car registry is not a Bruneian-Registered Car Plate</span>';
+    // $carplate = "CA2246";
+    
 
-    // SQL Statement 
-    $sql = "INSERT INTO bruneicarplate(carplate) VALUE ($carplate)";
+
+    // SQL Statement
+    $sql = "INSERT INTO bruneicarplate(carplate) VALUE ('$carplate')";
     if(mysqli_query($conn, $sql)){
         echo "New Record created successfully";
     }else{
         echo "Error: ".$sql. "</br>".mysqli_error($conn);
     }
+
+
 ?>
