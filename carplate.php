92ae34731fb8dbd4472b76ad5af87b34dd8cba16
Afwan Rezal <afwan.21b6027@gmail.com>
2023-04-10 09:16:09 +0800

    Transferred PHP script to carplate

--- carplate.php
+++ carplate.php
@@ -1,3 +1,22 @@
+<?php
+    require("db.php");
+    $carplate = $_POST["carplate"];
+    $carplat_err = '';
+    
+    
+    if(empty(trim($carplate))){
+        
+    }
+
+    // SQL Statement
+    $sql = "INSERT INTO bruneicarplate(carplate) VALUE ('$carplate')";
+    if(mysqli_query($conn, $sql)){
+        echo "New Record created successfully";
+    }else{
+        echo "Error: ".$sql. "</br>".mysqli_error($conn);
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,10 +26,11 @@
     <title>Bruneian Car Plate</title>
 </head>
 <body>
-    <form action="value.php" method="post">
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?>" method="post">
         <label for="carplate">Bruneian Car Plate</label><br>
         <input type="text" name="carplate" id="carplate">
         <input type="submit" value="submit">
+        <span></span>
 
         
     </form>
